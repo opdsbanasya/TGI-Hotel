d6254e5db4fe71fc636e7548418e1aac28084ac1
DHARM <opdsbanasya@gmail.com>
2024-10-16 20:19:26 +0530

    error fixing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src={LOGO} className="md:h-[4rem] h-14" alt="Flowbite Logo" />
                 </Link>
-                <button onClick={() => setShowMenu(!showMenu)} data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+                <button onClick={() => setShowMenu(!showMenu)} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                     <span className="sr-only">Open main menu</span>
                     {showMenu ? (
                         <img width="50" height="50" src="https://img.icons8.com/?size=100&id=6483&format=png&color=ffffff" alt="multiply" />
@@ -21,11 +21,11 @@ const Navbar = () => {
                     )}
 
                 </button>
-                <div id="navbar-hamburger" className="md:block hidden w-full md:w-auto">
+                <div className="md:block hidden w-full md:w-auto">
                     <ul className="font-medium flex flex-col p-4 md:p-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-black">
                         <li>
                             <Link to="/"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >Home</Link>
                         </li>
                         <li>
@@ -50,7 +50,7 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </div>
-                <div id="navbar-hamburger" className={`${showMenu ? "absolute right-0 top-20 text-center w-full md:w-auto" : "hidden"} md:hidden`}>
+                <div className={`${showMenu ? "absolute right-0 top-20 text-center w-full md:w-auto" : "hidden"} md:hidden`}>
                     <ul className="font-medium flex flex-col p-4 md:p-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-black">
                         <li>
                             <Link to="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">
