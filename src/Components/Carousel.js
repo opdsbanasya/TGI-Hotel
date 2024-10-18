14f5682fe32389402e364231fb51308629c1b160
Badre Alam <badre.2246162@mygyanvihar.com>
2024-10-18 13:02:08 +0530

    title of corousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CarouselBack from './CarouselBack';
+import CarouselTitle from './CarouselTitle';
 
 const Carousel = () => {
     return (
-        <div className='max-w-[90vw] max-h-[100vh] mx-auto'>
+        <div className='max-w-[90vw] max-h-[100vh] mx-auto relative'>
             <CarouselBack />
+            <CarouselTitle />
         </div>
     );
 }
---
+++ src/Components/CarouselTitle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CarouselTitle = () => {
+  return (
+    <div className='w-full absolute top-[50%] left-[8%] bg-gradient-to-r to-black text-white space-y-5'>
+        <h1 className='text-5xl font-bold'>TIG Hotel</h1>
+        <p className=' w-[40%]'>Our Company is a leading real estate company that specializes in providing high-end properties that redefine luxury and offer a sanctuary for a refined lifestyle. Our team of experts is committed to excellence, ensuring that each client receives personalized attention and exceptional service. We strive to exceed expectations and create exceptional living experiences.</p>
+        <button className='px-4 py-2 bg-blue-500 rounded-lg text-2xl font-bold'>Book</button>
+    </div>
+  )
+}
+
+export default CarouselTitle
