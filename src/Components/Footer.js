0236c5f3f664b5f4fa4922adc44d7eb834deb5ab
DHARM <opdsbanasya@gmail.com>
2024-10-20 00:25:55 +0530

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,33 +1,33 @@
 import { IoMdCall, IoMdMail } from "react-icons/io";
-import logo from "../assets/logo.jpg"
 import { FaFacebook, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 import { LOGO } from "../utils/constants";
 
 const Footer = () => {
     return (
-        <>
-            <div className="w-full py-10 px-28 flex justify-center items-center bg-gradient-to-r from-slate-950">
+        <div className="w-full bg-gradient-to-r from-slate-950">
+            <div className="w-full py-10 px-28 flex justify-center items-center ">
                 <div className="w-3/12">
                     <img src={LOGO} className="" />
                 </div>
                 <div className="w-4/12 space-y-5 text-xl">
                     <h4 className="text-2xl font-semibold">Contact Us</h4>
-                    <h4 className="flex items-center gap-3 mr-10 cursor-pointer"><IoMdMail className="bg-gray-400 size-6 p-1 rounded-full" />
+                    <h4 className="flex items-center gap-3 mr-10 cursor-pointer"><IoMdMail className="bg-white size-6 p-1 rounded-full" />
                         appleinn@tgihotels.com</h4>
-                    <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-gray-400 size-6 p-1 rounded-full" />
+                    <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-white size-6 p-1 rounded-full" />
                         +91 14 1486 0090 </h4>
-                    <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-gray-400 size-6 p-1 rounded-full" />
+                    <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-white size-6 p-1 rounded-full" />
                         180 0258 3100</h4>
                 </div>
                 <div className=" w-4/12 space-y-5 text-xl">
                     <h4 className="text-2xl font-semibold">Get In Touch</h4>
-                    <FaFacebook />
-                    <FaInstagram />
-                    <FaLinkedin />
-                    <FaXTwitter />
+                    <FaFacebook className="cursor-pointer" /> 
+                    <FaInstagram className="cursor-pointer" />
+                    <FaLinkedin className="cursor-pointer" />
+                    <FaXTwitter className="cursor-pointer" />
                 </div>
             </div>
-        </>
+            <h5 className="w-full text-lg text-white mx-10 ">All Copyright &copy; Reserves</h5>
+        </div> 
     );
 }
 
