2a4782db3d1ec062ff93a07e22f7e1e785904de7
DHARM <opdsbanasya@gmail.com>
2024-10-20 00:25:14 +0530

    icon style

--- src/Components/Address.js
+++ src/Components/Address.js
@@ -7,11 +7,11 @@ const Address = () => {
             <h2 className="text-4xl font-light font-serif">TGI Apple Inn, Jaipur</h2>
             <p className="">E-7, 36, Nirman, Nagar DCM Ajmer Road, Jaipur, Rajasthan - 302019</p>
             <div className="flex justify-center gap-8">
-                <h4 className="flex items-center gap-3 mr-10 cursor-pointer"><IoMdMail className="bg-gray-400 size-6 p-1 rounded-full" />                     
+                <h4 className="flex items-center gap-3 mr-10 cursor-pointer"><IoMdMail className="bg-black text-white size-6 p-1 rounded-full" />                     
                     appleinn@tgihotels.com</h4>
-                <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-gray-400 size-6 p-1 rounded-full" />
+                <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-black text-white size-6 p-1 rounded-full" />
                     +91 14 1486 0090 </h4>
-                <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-gray-400 size-6 p-1 rounded-full" />
+                <h4 className="flex items-center gap-3 cursor-pointer"><IoMdCall className="bg-black text-white size-6 p-1 rounded-full" />
                     180 0258 3100</h4>
             </div>
             <div className="flex justify-center gap-5 text-sm">
