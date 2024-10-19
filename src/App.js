83d98e75a391180f862dbaa469752ef2c8b2a2a5
DHARM <opdsbanasya@gmail.com>
2024-10-19 19:22:41 +0530

    fixing route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./Components/Body";
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom';
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+    return <div className=" font-[poppins]">
+        <Navbar />
+        <Outlet />
+    </div>
+}
 
 const appRouter = createBrowserRouter([
     {
-        path: '/',
-        element: <Body />
-    }
-]);
-
-
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            }
+        ]
+    },
+    
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <RouterProvider router={appRouter} />
-);
+root.render(<RouterProvider router={appRouter} />);
