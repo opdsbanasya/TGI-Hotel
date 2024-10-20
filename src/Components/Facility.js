e693d8640d81073944570278c3c3e19e855a29e6
DHARM <opdsbanasya@gmail.com>
2024-10-21 00:01:08 +0530

    facility

--- src/Components/Facility.js
+++ src/Components/Facility.js
@@ -1,7 +1,7 @@
 
 const Facility = ({item}) => {
     return (
-        <div className="w-[32%] relative overflow-hidden rounded-lg">
+        <div className="w-[32%] relative overflow-hidden rounded-lg cursor-pointer">
             <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
             <img className="w-full aspect-[4/3] object-cover" src={item.img} alt="In-House Bar" />
             <p className="absolute left-5 bottom-2 text-white">{item.title}</p>
