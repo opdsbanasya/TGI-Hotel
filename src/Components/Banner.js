f78f6c6c534033abfc2879bb4317d2b3a666f289
DHARM SINGH SAINI <opdsbanasya@gmail.com>
2024-10-25 22:57:10 +0530

    contact us responsive

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,7 +3,7 @@ import banner from "../assets/building-view.jpg"
 
 const Banner = ({ bannerImg }) => {
     return (
-        <div className='w-full h-[80vh] mx-auto overflow-hidden rounded-xl'>
+        <div className='w-full h-[25vh] sm:h-[30vh] lg:h-[40vh] xl:h-[80vh] md:h-[35vh] mx-auto overflow-hidden md:rounded-xl rounded-lg'>
             <img className="w-full h-full scale-110 object-cover object-bottom" src={bannerImg ? bannerImg : banner} />
         </div>
     );
--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -16,88 +16,41 @@ const ContactUs = () => {
 
 
     return (
-        <div className="w-full min-h-[50vh] px-28 py-7">
+        <div className="w-full min-h-[50vh] px-5 md:px-10 lg:px-20 xl:px-28 py-7">
             <Banner />
             <ContactInfo />
-            <h2 className="w-[80%] text-3xl mx-28 font-semibold font-serif text-black my-10">
-                Reach out to Us
-            </h2>
-            <div className='flex items-center justify-center bg-zinc-300 rounded-xl overflow-hidden'>
-                <div className='w-1/2 '>
-                    <img className='aspect-square object-cover' src={mainEntrance} alt='Main Entrance' />
+            <div className="text-center lg:text-left xl:w-[80%] w-[90%] mx-auto">
+                <h2 className="lg:text-3xl text-2xl lg:mx-0 mx-auto font-semibold font-serif text-black lg:my-10 py-5 mt-3">
+                    Reach out to Us
+                </h2>
+            </div>
+            <div id="contact-form" className="flex flex-col lg:flex-row items-center justify-center bg-zinc-300 rounded-xl overflow-hidden">
+                <div className="lg:w-1/2 w-full h-full">
+                    <img className="aspect-square object-cover h-full" src={mainEntrance} alt="Main Entrance" />
                 </div>
-                <form onSubmit={(e) => handleSubmit(e, formData, setFormData, setErrors)} className="w-1/2 space-y-4  mx-auto p-10">
-                    <div className=' gap-10'>
-                        {/* <label for="name" className='text-xl w-32'>Name: </label> */}
-                        <input
-                            type="text"
-                            name="name"
-                            id='name'
-                            value={formData.name}
-                            onChange={() => handleChange(e, setFormData)}
-                            placeholder="Name"
-                            className={`w-full px-4 py-3 rounded-md border ${errors.name ? 'border-red-500' : 'border-gray-300'
-                                } focus:outline-none `}
-                        />
-                        {errors.name && (
-                            <p className="text-red-500 text-xs mt-1">{errors.name}</p>
-                        )}
-                    </div>
-
-                    <div className=' gap-10'>
-                        {/* <label for="email" className='text-xl w-32'>Email: </label> */}
-                        <input
-                            type="email"
-                            name="email"
-                            id='email'
-                            value={formData.email}
-                            onChange={() => handleChange(e, setFormData)}
-                            placeholder="Email"
-                            className={`w-full px-4 py-3 rounded-md border ${errors.email ? 'border-red-500' : 'border-gray-300'
-                                } focus:outline-none `}
-                        />
-                        {errors.email && (
-                            <p className="text-red-500 text-xs mt-1">{errors.email}</p>
-                        )}
-                    </div>
-
-                    <div className=' gap-10'>
-                        {/* <label for="phone" className='text-xl w-32'>Phone: </label> */}
-                        <input
-                            type="tel"
-                            name="phone"
-                            id='phone'
-                            value={formData.phone}
-                            onChange={() => handleChange(e, setFormData)}
-                            placeholder="Phone No"
-                            className={`w-full px-4 py-3 rounded-md border ${errors.phone ? 'border-red-500' : 'border-gray-300'
-                                } focus:outline-none `}
-                        />
-                        {errors.phone && (
-                            <p className="text-red-500 text-xs mt-1">{errors.phone}</p>
-                        )}
-                    </div>
-
-                    <div className=' gap-10'>
-                        {/* <label for="message" className='text-xl w-32'>Message: </label> */}
-                        <textarea
-                            name="message"
-                            id='message'
-                            value={formData.message}
-                            onChange={() => handleChange(e, setFormData)}
-                            placeholder="Message"
-                            rows="4"
-                            className={`w-full px-4 py-3 rounded-md border ${errors.message ? 'border-red-500' : 'border-gray-300'
-                                } focus:outline-none  resize-none`}
-                        />
-                        {errors.message && (
-                            <p className="text-red-500 text-xs mt-1">{errors.message}</p>
-                        )}
-                    </div>
-
+                <form onSubmit={(e) => handleSubmit(e, formData, setFormData, setErrors)} className="lg:w-1/2 w-full md:[80%] space-y-6 mx-auto px-10 py-5">
+                    <h2 className="lg:text-lg text-sm font-semibold font-serif text-black">
+                        Please! Fill your original Info, so we can reply to you.
+                    </h2>
+                    {['name', 'email', 'phone', 'message'].map((field, index) => (
+                        <div key={field} className="flex flex-col gap-2">
+                            <input
+                                type={field === 'message' ? 'textarea' : field}
+                                name={field}
+                                id={field}
+                                value={formData[field]}
+                                onChange={(e) => handleChange(e, setFormData)}
+                                placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                                className={`w-full px-4 py-3 rounded-md border ${errors[field] ? 'border-red-500' : 'border-gray-300'} focus:outline-none`}
+                            />
+                            {errors[field] && (
+                                <p className="text-red-500 text-xs mt-1">{errors[field]}</p>
+                            )}
+                        </div>
+                    ))}
                     <button
                         type="submit"
-                        className="w-fit mx-auto bg-[#4CC9FE] text-white py-3 px-4 rounded-md hover:bg-[#37AFE1] transition duration-300 focus:outline-none  focus:ring-offset-2"
+                        className="w-fit mx-auto bg-[#4CC9FE] text-white py-3 px-4 rounded-md hover:bg-[#37AFE1] transition duration-300 focus:outline-none focus:ring-offset-2"
                     >
                         Send Message
                     </button>
