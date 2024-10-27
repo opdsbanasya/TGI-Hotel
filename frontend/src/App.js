77165b202223ee8287bf270390e0e038b6174eb1
DHARM <opdsbanasya@gmail.com>
2024-10-28 00:17:07 +0530

    bottom to top button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./Components/Body";
-import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Facilities from "./Components/Facilities";
 import About from "./Components/About";
 import ContactUs from "./Components/ContactUs";
 import Pricing from "./Components/Pricing";
+import { FaAngleUp } from "react-icons/fa6";
 
 const App = () => {
-    return <div className="bg-[#FEFAE0] font-[poppins]">
+
+    const scrollToUp = ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    return <div className="bg-[#FEFAE0] font-[poppins] relative">
         <Navbar />
         <Outlet />
         <Footer />
+        <div onClick={scrollToUp} className="size-fit fixed right-5 bottom-5 bg-zinc-200 rounded-full text-3xl p-2 cursor-pointer">
+            <FaAngleUp />
+        </div>
     </div>
 }
 
@@ -46,7 +58,7 @@ const appRouter = createBrowserRouter([
             },
         ]
     },
-    
+
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
