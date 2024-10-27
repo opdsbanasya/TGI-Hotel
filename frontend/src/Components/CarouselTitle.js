fc53c20821bc70282400e858f508c89c18455aff
DHARM <opdsbanasya@gmail.com>
2024-10-27 23:11:14 +0530

    responsive

--- frontend/src/Components/CarouselTitle.js
+++ frontend/src/Components/CarouselTitle.js
@@ -6,8 +6,12 @@ const CarouselTitle = () => {
 
   const navigate = useNavigate();
 
-  const navigateContactUs = () => {
+  const navigateContactUs = (id) => {
     navigate("/contact-form");
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   }
 
   return (
@@ -16,7 +20,7 @@ const CarouselTitle = () => {
       <p className=' w-[40%] font-light tracking-wide'>{CAROUSEL_DATA?.about}</p>
       <button
         className='cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]'
-        onClick={navigateContactUs}
+        onClick={() => navigateContactUs("contact-form")}
       >Reach out</button>
     </div>
   )
--- frontend/src/Components/Facilities.js
+++ frontend/src/Components/Facilities.js
@@ -8,7 +8,7 @@ const Facilities = () => {
     return (
         <div className="w-full min-h-[50vh] px-5 md:px-10 lg:px-20 xl:px-28 lg:pb-10 py-7">
             <Banner bannerImg={buildingImg} />
-            <h3 className="w-full md:w-[90%] text-lg md:text-2xl lg:text-3xl xl:text-4xl font-light mx-auto text-center font-serif py-7 md:py-10">Facilities at TGI Grand Fortuna, Hosur</h3>
+            <h2 className="w-full md:w-[90%] text-lg md:text-2xl lg:text-3xl xl:text-4xl font-light mx-auto text-center font-serif py-7 md:py-10">Facilities at TGI Grand Fortuna, Hosur</h2>
             <div className="flex items-center justify-center xl:gap-5 lg:gap-10 md:gap-8 gap-5 flex-wrap">
                 {FACILITIES_DATA.map((item) => (
                     <Facility key={item.id} item={item} />
